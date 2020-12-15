77b9292778cb3c27f45c7751f54ecdcd88de1455
Jumpybuns <ethanpiercepresents@gmail.com>
2020-12-14 18:28:32 -0800

    i dont think Im done but I dont know what eles to do

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('app routes', () => {
-
-});
+// describe('app routes', () => {
+//   it('GET status code 200', async() => {
+//     const response = await request(app)
+//       .get('/');
+    
+//     expect(response.body).toEqual('hi');
+//   });
+// });
